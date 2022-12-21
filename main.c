9bf34abc8bb1f9b9e8509fc2965d3f2b35cc44e6
Jason Betts <jabetts1@gmail.com>
2022-12-21 16:42:56 +1100

    Added bounds checking

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 #define MAX_STRING 502
 
 /* simple bubble sort */
-void sort(int arr[])
+void sort(int arr[], size_t n)
 {
     int i, j;
 
-    for(i = 0; i < 26; ++ i) {
-        for(j = 0; j < 26 - 1; ++j) {
+    for(i = 0; i < n; ++ i) {
+        for(j = 0; j < n - 1; ++j) {
 
             if(arr[j] < arr[j + 1]) {
                 int temp = arr[j+1];
@@ -31,17 +31,17 @@ int calc(char *str)
 
     int arr[26] = {0};
 
-    while(str[i] != '\n' && str[i]) {
+    while (str[i] != '\n' && str[i]) {
         if(isalpha(str[i])) {
-            char temp = tolower(str[i]) - 97;
+            char temp = tolower(str[i]) - 97; // get index of the letter, a = 0, b = 2 etc.
             arr[temp] += 1;
         }
         ++i;
     }
 
-    sort(arr);
+    sort(arr, 26);
 
-    for(i = 0, n = 26; arr[i] != 0; ++i) {
+    for (i = 0, n = 26; arr[i] != 0 && i < 26; ++i) {
         r += arr[i] * n--;
     }
 
@@ -50,11 +50,12 @@ int calc(char *str)
 
 int main(int argc, char **argv)
 {
+    int i = 0;
     if (argc < 2) {
         printf("usage: main <filename>\n");
         return -1;
     }
-    
+
     FILE *file = fopen(argv[1], "r");
     if(!file) {
         printf("Unable to open %s", argv[1]);
@@ -63,8 +64,10 @@ int main(int argc, char **argv)
 
     char  line[502];
 
-    while(fgets(line, MAX_STRING, file))
-        printf("%d\n", calc(line));
+    while(fgets(line, MAX_STRING, file)) {
+        printf("Case #%d: %d\n", ++i, calc(line));
+
+    }
 
     return 0;
 }
