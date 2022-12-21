3908b3d33a52712e8609c993f0b37bdbed9770a5
Jason Betts <jabetts1@gmail.com>
2022-12-21 16:07:31 +1100

    Added input file check

--- main.c
+++ main.c
@@ -50,6 +50,11 @@ int calc(char *str)
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("usage: main <filename>\n");
+        return -1;
+    }
+    
     FILE *file = fopen(argv[1], "r");
     if(!file) {
         printf("Unable to open %s", argv[1]);
